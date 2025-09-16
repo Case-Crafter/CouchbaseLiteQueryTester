ce6bfcfaea05945c1dc5bb4d5ced17bcfa74f1fb
Tor Øyvind Fluør <tor@timelinesystems.net>
2025-09-16 09:51:03 +0200

    Added simple UI

---
+++ CouchbaseLiteQueryTester/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CouchbaseLiteQueryTester
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CouchbaseLiteQueryTester/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CouchbaseLiteQueryTester")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CouchbaseLiteQueryTester.Pages")]
